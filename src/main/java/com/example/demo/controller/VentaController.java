f419531f5e482fecf106c6907bc380cd63ef69ea
cristian <cristiannrosales12@gmail.com>
2024-06-07 12:17:24 -0300

    Cambios sobre venta

--- src/main/java/com/example/demo/controller/VentaController.java
+++ src/main/java/com/example/demo/controller/VentaController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
+import com.example.demo.entities.Articulo;
 import com.example.demo.entities.Cliente;
 import com.example.demo.entities.Venta;
+import com.example.demo.repositories.ArticuloRepository;
 import com.example.demo.repositories.ClienteRepository;
 import com.example.demo.services.VentaServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +22,8 @@ public class VentaController extends BaseControllerImpl<Venta, VentaServiceImpl>
     VentaServiceImpl service;
     @Autowired
     ClienteRepository ClienteRepo;
+    @Autowired
+    ArticuloRepository ArticuloRepo;
 
     //este metodo listaria todas las ventas que estan cargadas
     @GetMapping("/disponibles")
@@ -34,9 +38,11 @@ public class VentaController extends BaseControllerImpl<Venta, VentaServiceImpl>
     public String CargarNueva(Model model){
 
         List<Cliente> clientes = ClienteRepo.findAll();
+        List<Articulo> articulos = ArticuloRepo.findAllDisponibles();
 
-        //te dejaria buscar el cliente al cual hacerle la venta
+        //te dejaria buscar el cliente al cual hacerle la venta con los productos
         model.addAttribute("clientes",clientes);
+        model.addAttribute("articulos",articulos);
 
         return "vistaedicion"; //falta esto
     }
--- src/main/java/com/example/demo/repositories/ArticuloRepository.java
+++ src/main/java/com/example/demo/repositories/ArticuloRepository.java
@@ -2,9 +2,15 @@ package com.example.demo.repositories;
 
 import com.example.demo.entities.Articulo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ArticuloRepository extends BaseRepository<Articulo,Long> {
 
+
+    @Query(value = "select * from articulo A where A.StockSeguridad>0 and A.CantActual>0 and A.fechaBaja is null", nativeQuery = true)
+    List<Articulo> findAllDisponibles();                    //como se haria el manejo de cantActual y stock de seguridad??
 }
