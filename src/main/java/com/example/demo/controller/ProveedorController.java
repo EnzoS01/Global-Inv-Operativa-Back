1d91b06d91c21acc0848d2a83f53da3f5216d0e3
alear12373 <alear12373@gmail.com>
2024-06-08 12:08:18 -0300

    arreglo Proveedorcontroller

--- src/main/java/com/example/demo/controller/ProveedorController.java
+++ src/main/java/com/example/demo/controller/ProveedorController.java
@@ -1,5 +1,17 @@
 package com.example.demo.controller;
 
-public class ProveedorController {
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.Proveedor;
+import com.example.demo.entities.ProveedorArticulo;
+import com.example.demo.services.ProveedorArticuloServiceImpl;
+import com.example.demo.services.ProveedorServiceImpl;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping(path = "api/proveedores")
+public class ProveedorController extends BaseControllerImpl<Proveedor,ProveedorServiceImpl>{
 
 }
