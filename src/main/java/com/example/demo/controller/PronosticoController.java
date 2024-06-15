3142171a8dae78f2166b3e6a045884f339d759f6
Ignacio Olcoz <ignacio.olcoz@alumnos.frm.utn.edu.ar>
2024-06-14 23:13:07 -0300

    Se quitan imports no utilizados.

--- src/main/java/com/example/demo/controller/PronosticoController.java
+++ src/main/java/com/example/demo/controller/PronosticoController.java
@@ -1,14 +1,9 @@
 package com.example.demo.controller;
 
 import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
 import com.example.demo.entities.Demanda;
 import com.example.demo.entities.DemandaPronosticada;
-import com.example.demo.entities.Pronostico;
 import com.example.demo.services.PronosticoServiceImpl;
-import com.example.demo.repositories.DemandaPronosticadaRepository;
-import com.example.demo.repositories.PronosticoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
