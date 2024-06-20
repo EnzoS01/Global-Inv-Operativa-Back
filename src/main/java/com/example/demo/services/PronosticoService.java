2eece233917da6ee79cb6924ba82a7714cd31340
Ignacio Olcoz <ignacio.olcoz@alumnos.frm.utn.edu.ar>
2024-06-20 17:45:17 -0300

    Update PronosticoServiceImpl.java

--- src/main/java/com/example/demo/services/PronosticoService.java
+++ src/main/java/com/example/demo/services/PronosticoService.java
@@ -9,26 +9,20 @@ import java.util.List;
 public interface PronosticoService extends BaseService<Pronostico, Long> {
     List<Demanda> seleccionarDemandaHistoricas(List<Demanda> listaDemandasHistoricas, int nroAnio);
 
-    List<DemandaPronosticada> promedioMovil(List<Demanda> demandasHistoricasSeleccionadas, int n, int cantidadPeriodosAPredecir);
+    List<DemandaPronosticada> promedioMovil(List<Demanda> demandasHistoricas);
 
-    List<DemandaPronosticada> promedioPonderado(List<Demanda> demandasHistoricasSeleccionadas, int n, int cantidadPeriodosAPredecir);
+    List<DemandaPronosticada> promedioPonderado(List<Demanda> demandasHistoricas);
 
-    List<DemandaPronosticada> suavizacionExponencial(List<Demanda> demandasHistoricasSeleccionadas, double alpha, int mesDemandaAnterior, int cantidadRealDemandadaAnterior, int cantidadDemandadaPronosticadaAnterior);
+    List<DemandaPronosticada> suavizacionExponencial(List<Demanda> demandasHistoricas);
 
-    List<DemandaPronosticada> pronosticoEstacional(List<Demanda> demandasHistoricas, int anioAPredecir, int anioInicio, int anioFin, int demandaEsperada);
+    List<DemandaPronosticada> pronosticoEstacional(List<Demanda> demandasHistoricas);
 
     List<DemandaPronosticada> predecirDemanda(List<Demanda> demandas);
 
-    DemandaPronosticada getPronostico(Long id);
+    double obtenerDesviacionAbsolutaMedia(List<DemandaPronosticada> demandasPronosticadas);
 
-    DemandaPronosticada updatePronostico(Long id, DemandaPronosticada pronosticoActualizado);
+    double obtenerErrorMedioAbsolutoPorcentual(List<DemandaPronosticada> demandasPronosticadas);
 
-    void deletePronostico(Long id);
-
-    List<DemandaPronosticada> getAllPronosticos();
-
-    List<DemandaPronosticada> getDemandasPronosticadasByPronosticoId(Long id);
-
-
+    double obtenerErrorCuadraticoMedio(List<DemandaPronosticada> demandasPronosticadas);
 
 }
