88593a912f470617edd1e267d6d14d83229550cb
cristian <cristiannrosales12@gmail.com>
2024-06-27 10:20:49 -0300

    Actualizo query orden de compra

--- src/main/java/com/example/demo/repositories/OrdenCompraRepository.java
+++ src/main/java/com/example/demo/repositories/OrdenCompraRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface OrdenCompraRepository extends BaseRepository<OrdenCompra,Long>{
 
-    @Query(value = "select * from ordenCompra o where o.fk_estadoOrdenCompra = :idEstado1 or o.fk_estadoOrdenCompra = :idEstado2", nativeQuery = true)
+    @Query(value = "select * from ORDEN_COMPRA o where o.FK_ESTADO_ORDEN_COMPRA = :idEstado1 or o.FK_ESTADO_ORDEN_COMPRA = :idEstado2", nativeQuery = true)
     List<OrdenCompra> findByState(@Param("idEstado1") Long idEstado1, @Param("idEstado2") Long idEstado2);
 
     @Query(value = "select o.* from ordenCompra o join detalleOrdenCompra d on o.id = d.ordenCompra_id "+
