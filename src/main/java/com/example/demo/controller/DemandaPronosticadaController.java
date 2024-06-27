1a5f1b1aabc41c1f1ee2ad27f1e05c0f95dc0534
EnzoS01 <cocenzosilva@gmail.com>
2024-06-26 21:11:06 -0300

    Update DemandaPronosticadaController.java

--- src/main/java/com/example/demo/controller/DemandaPronosticadaController.java
+++ src/main/java/com/example/demo/controller/DemandaPronosticadaController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
+import com.example.demo.entities.Articulo;
 import com.example.demo.entities.DemandaPronosticada;
+import com.example.demo.services.ArticuloServiceImpl;
 import com.example.demo.services.DemandaPronosticadaService;
+import com.example.demo.services.DemandaPronosticadaServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,8 +14,8 @@ import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping("api/demandaspronosticada")
-public class DemandaPronosticadaController {
+@RequestMapping("api/demandaspronosticadas")
+public class DemandaPronosticadaController extends BaseControllerImpl<DemandaPronosticada, DemandaPronosticadaServiceImpl>{
 
     @Autowired
     private DemandaPronosticadaService demandaPronosticadaService;
