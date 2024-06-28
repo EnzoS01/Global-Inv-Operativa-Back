e30daf4cce45e0c0b2a380cdffe1f42aa87ac17e
cristian <cristiannrosales12@gmail.com>
2024-06-28 18:23:54 -0300

    Problema query ejecución

--- src/main/java/com/example/demo/repositories/ProveedorRepository.java
+++ src/main/java/com/example/demo/repositories/ProveedorRepository.java
@@ -7,10 +7,16 @@ import org.springframework.stereotype.Repository;
 
 import com.example.demo.entities.Proveedor;
 
+import java.util.List;
+
 @Repository
 public interface ProveedorRepository extends BaseRepository<Proveedor,Long>{
 
     @Query(value = "select * from  PROVEEDOR p where p.nombre = :nombre ", nativeQuery = true)
     Proveedor findByName(@Param("nombre") String nombre);
+
+    @Query(value = "SELECT pa.proveedor FROM ProveedorArticulo pa WHERE pa.articulo.id = :idArticulo")
+    List<Proveedor> findProveedoresByArticuloId(@Param("idArticulo") Long idArticulo);
+
 
 }
